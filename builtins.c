6658ccdeedb14ba8fb6bdc05d4584b603ac06a4e
stefansilverio <494@holbertonschool.com>
2018-11-16 19:25:54 +0000

    builtin fn modified to handle cd

--- builtins.c
+++ builtins.c
@@ -8,13 +8,18 @@ int run_builtin(char **tokenarr)
 {
 	int index = 0;
 	builtin_t cmds[] = {
-		{"exit", exit_sash}, {"env", _env}, {NULL, NULL}
+		{"exit", exit_sash}, {"env", _env}, {"cd", _chdir}, {NULL, NULL}
 	};
 
 	while (cmds[index].cmd)
 	{
 		if (str_cmp(tokenarr[0], cmds[index].cmd) == 0)
+		{
+			printf("%c\n", cmds[2].cmd[1]);
+			if (cmds[2].cmd[1] == 'd')
+				return(cmds[index].func(tokenarr));
 			return(cmds[index].func());
+		}
 		index++;
 	}
 	return (-1);
