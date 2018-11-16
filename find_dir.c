4e420a7b03dd06ca7a51fce1ae123732a9cef146
jozsa <allisonjoyweiner@gmail.com>
2018-11-16 02:14:09 +0000

    Shortened function and changed return to NULL upon failure

--- find_dir.c
+++ find_dir.c
@@ -1,39 +1,6 @@
 #include "sash.h"
 
 /**
- * tokenize_path - Break down the PATH into tokens
- * @path: The path to be tokenized
- *
- * Return: A pointer to an array of pointers to tokens
- */
-
-char **tokenize_path(char *path)
-{
-	char *token;
-	char *delim = ":";
-	char **buf_dir = NULL;
-	int idx = 0;
-	int count = 0;
-
-	while (path[idx])
-	{
-		if (path[idx] == *delim)
-			count++;
-		idx++;
-	}
-
-	buf_dir = malloc(sizeof(char *) * (count + 1));
-	idx = 0;
-	token = strtok(path, delim);
-	while (token)
-	{
-		buf_dir[idx] = token;
-		token = strtok(NULL, delim);
-		idx++;
-	}
-	return (buf_dir);
-}
-/**
  * find_dir - check the usrs path for executable
  * @buf_dir: An array of pointers to path tokens
  * @command: The command/executable given to the shell
@@ -46,22 +13,17 @@ char *find_dir(char **buf_dir, char *command)
 	int chr_cnt =  0; /* chrs in path dir */
 	int cmd_len = 0; /* len of command usr enters */
 	int buf_size = 0; /* space you need allocated */
-	int dir_idx = 0, b_idx = 0;
+	int dir_idx = 0;
+	int b_idx = 0;
 	struct stat buff;
 
 	while (buf_dir[dir_idx] != NULL)
 	{
-		cmd_len = 0;
-		chr_cnt = 0;
-
-		while (buf_dir[dir_idx][chr_cnt] != '\0')
-			chr_cnt++; /* get length of next dir */
-		while (command[cmd_len] != '\0')
-			cmd_len++; /* len of tokenarr[0] */
+		cmd_len = doubleptr_len(buf_dir, dir_idx);
+		chr_cnt = str_len(command);
 		buf_size = cmd_len + chr_cnt;
-		buffer = malloc((sizeof(char)) * (buf_size));
 
-		cmd_len = 0;
+		buffer = malloc((sizeof(char)) * (buf_size));
 		chr_cnt = 0;
 
 		for (b_idx = 0; buf_dir[dir_idx][chr_cnt] != '\0'; b_idx++)
@@ -71,14 +33,16 @@ char *find_dir(char **buf_dir, char *command)
 		}
 		buffer[b_idx] = '/';
 		b_idx++;
+		cmd_len = 0;
 		while (command[cmd_len] != '\0')
 		{
-			buffer[b_idx + cmd_len] = command[cmd_len];
+			buffer[b_idx] = command[cmd_len];
+			b_idx++;
 			cmd_len++; /* len of tokenarr[0] */
 		}
 		if (stat(buffer, &buff) == 0)
 			return (buffer);
 		dir_idx++;
 	}
-	return (0);
+	return (NULL);
 }
