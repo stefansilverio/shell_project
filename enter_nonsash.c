90c9c65af3a8f3ad6dfc2d3687da418ae550e6b7
stefansilverio <494@holbertonschool.com>
2018-11-19 22:54:42 +0000

    new enter_nonsash

--- enter_nonsash.c
+++ enter_nonsash.c
@@ -4,39 +4,49 @@ int enter_nonsash(int ac, char **av, char **env)
 {
 	char *line = NULL, *path = NULL, *dir = NULL;
 	char **commands = NULL, **dirs = NULL;
+	blocks_s *head_s = NULL, *first_node_s = NULL;
+	blocks_d *head_d = NULL, *first_node_d = NULL;
 	int status = 1;
 	struct stat buffer;
 	char *prev = NULL, *buff = NULL;
-
+	char *blk0 = "blk0", *blk1 = "blk1", *blk2 = "blk2", *blk3 = "blk3", *blk4 = "blk4";
 	(void)ac;
+	(void)**av;
 
-	prev = getcwd(buff, 0);
-	
+	prev = getcwd(buff, 1024);
+
 	while (status)
 	{
 		line = read_line();
 		if (line == NULL)
 			continue;
+		head_s = build_list_s(head_s, line, blk0);
+		first_node_s = head_s;
 		commands = split_line(line, av);
+		head_d = build_list_d(head_d, commands, blk1);
+		first_node_d = head_d;
 		status = run_builtin(commands);
 		if (status == -1)
 		{
-			if (!str_chr(commands[0], '/'))
+			path = get_path(env);
+			head_s = build_list_s(head_s, path, blk2);
+			if (!str_str(path, prev))
 			{
+				chdir("/home/vagrant");
 				if (stat(commands[0], &buffer) != 0)
 				{
-					chdir("/home/vagrant");
-					path = get_path(env);
 					dirs = tokenize_path(path);
+					head_d = build_list_d(head_d, dirs, blk3);
 					dir = find_dir(dirs, commands[0]);
+					head_s = build_list_s(head_s, dir, blk4);
 					commands[0] = dir;
-					chdir(prev);
 				}
+				chdir(prev);
 			}
 			_execute(commands[0], commands, env);
+			frees_s(first_node_s);
+			frees_d(first_node_d);
 		}
-		free(commands);
-		free(line);
 	}
 	return (0);
 }
