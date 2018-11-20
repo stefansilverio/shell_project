15ead0cd4aded9f78b4d0492d6a3c99bb4bd226f
jozsa <allisonjoyweiner@gmail.com>
2018-11-20 03:47:51 +0000

    Betty-style fixes - Added description

--- free_single_list.c
+++ free_single_list.c
@@ -1,5 +1,10 @@
 #include "sash.h"
 
+/**
+ * frees_s - Frees all allocated single pointers added to linked list
+ * @head: The first node of the linked list
+ */
+
 void frees_s(blocks_s *head)
 {
 	blocks_s *tmp;
