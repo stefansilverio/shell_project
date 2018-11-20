3535ae75ee2fe0c6dd8d1e1cb4d0ac95aff2e3fa
jozsa <allisonjoyweiner@gmail.com>
2018-11-20 03:43:14 +0000

    Fixed prototype header and freeing stuff

--- enter_sash.c
+++ enter_sash.c
@@ -9,11 +9,10 @@ int enter_sash(int ac, char **av, char **env)
 {
 	char *line = NULL, *path = NULL, *dir = NULL;
 	char **commands = NULL, **dirs = NULL;
-/*	blocks_s *head_s = NULL, *first_node_s = NULL;
-	blocks_d *head_d = NULL, *first_node_d = NULL;*/
+	blocks_s *head_s = NULL, *first_node_s = NULL;
+	blocks_d *head_d = NULL, *first_node_d = NULL;
 	int status = 1;
 	struct stat buffer;
-/*	char *blk0 = "blk0", *blk1 = "blk1", *blk2 = "blk2", *blk3 = "blk3", *blk4 = "blk4"; */
 	(void) ac;
 
 	signal(SIGINT, sighandler);  /* Ignores Ctrl+C signal to quit shell */
@@ -24,29 +23,28 @@ int enter_sash(int ac, char **av, char **env)
 		line = read_line(); /* Stores command written in line */
 		if (line == NULL)
 			continue;
-/*		head_s = build_list_s(head_s, line);
-		first_node_s = head_s; */
 		commands = split_line(line, av);
-/*		head_d = build_list_d(head_d, commands);
-		first_node_d = head_d; */
+		head_d = build_list_d(head_d, commands);
+		first_node_d = head_d;
 		status = run_builtin(commands);
 		if (status == -1)
 		{
 			if (stat(commands[0], &buffer) != 0)
 			{
 				path = get_path(env);
-/*				head_s = build_list_s(head_s, path);*/
+				head_s = build_list_s(head_s, path);
+				first_node_s = head_s;
 				dirs = tokenize_path(path);
-/*				head_d = build_list_d(head_d, dirs); */
+				head_d = build_list_d(head_d, dirs);
 				dir = find_dir(dirs, commands[0]);
-/*				head_s = build_list_s(head_s, dir);*/
+				head_s = build_list_s(head_s, dir);
 				commands[0] = dir;
 			}
 			_execute(commands[0], commands, env);
-/*			frees_s(first_node_s);
-			frees_d(first_node_d); */
 		}
 	}
+	frees_s(first_node_s);
+	frees_d(first_node_d);
 	return (0);
 }
 
