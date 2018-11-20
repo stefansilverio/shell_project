e9851eae853f9e77009071f96d257d78d5f7178e
stefansilverio <494@holbertonschool.com>
2018-11-20 01:57:08 +0000

    get length of strings

--- str_len.c
+++ str_len.c
@@ -1,3 +1,11 @@
+#include "sash.h"
+
+/**
+ * str_len - Counts the length of a string
+ * @command: The string to be counted
+ *
+ * Return: The length of the counted string
+ */
 int str_len(char *command)
 {
 	int cmd_len = 0;
