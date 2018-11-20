6cc311ac2b14b37a165efb9c60cbe168dafc1ddf
stefansilverio <494@holbertonschool.com>
2018-11-20 02:06:20 +0000

    create double pointer list

--- double_ptr_list.c
+++ double_ptr_list.c
@@ -1,27 +1,27 @@
 #include "sash.h"
 
-blocks_d *build_list_d(blocks_d *head, char** memory, char *num)
+/**
+ * doubleptr_len - Counts the number of characters of each string in a 2D array
+ * @buf_dir: The 2D array to be counted
+ * @dir_idx: The index of the current string in the 2D array
+ *
+ * Return: The character count of the string
+ */
+blocks_d *build_list_d(blocks_d *head, char** memory)
 {
 	blocks_d *new_node;
 
 	new_node = malloc(sizeof(blocks_d)); /* allocate space for new node */
 
 	new_node->memory = memory;
-	new_node->num = num;
 	new_node->next = NULL;
 
 	if (head == NULL)
-	{
 		head = new_node;
-		printf("%s\n", num);
-/*		printf("%p\n", new_node->(*memory));*/
-	}
 	else
 		while (head != NULL)
 		{
 			head = new_node->next;
-			printf("%s\n", new_node->num);
-/*			printf("%p\n", new_node->(*memory)); */
 		}
 
 	return (new_node); /* return ptr to start */
