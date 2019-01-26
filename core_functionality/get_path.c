ef8e24b686af109f5ae985a30ad01449446822cb
stefansilverio <494@holbertonschool.com>
2019-01-26 06:35:10 +0000

    core functionality

---
+++ core_functionality/get_path.c
@@ -0,0 +1,52 @@
+#include "sash.h"
+
+/**
+ * get_path - Get the PATH environmental variable and store it in a pointer
+ * @env: The environment to get the PATH from
+ *
+ * Return: A pointer to the buffer containing the variable
+ */
+
+char *get_path(char **env)
+{
+	int key_idx = 0, b_idx = 0, env_value = 5, cwd_idx = 0;
+	char *buffer = NULL, *curr = NULL, *buff = NULL;
+
+	curr = getcwd(buff, 0);
+	buffer = malloc(sizeof(char) * (1024)); /* buffer size is arbitrary */
+	if (buffer == NULL)
+		return (NULL);
+	while ((*(env[key_idx]) != 'P') && (*(env[key_idx] + 3) != 'H'))
+		key_idx++;
+	if (*(env[key_idx] + env_value) == ':')
+	{
+		env_value = env_value + 1;
+		str_cat(buffer, curr);
+	}
+	while (*(env[key_idx] + env_value) != '\0')
+	{
+		if (*(env[key_idx] + env_value) == ':')
+		{
+			if (*(env[key_idx] + (env_value + 1)) == ':')
+			{
+				env_value = env_value + 2;
+				buffer--;
+				str_cat(buffer, curr);
+			}
+		}
+		b_idx = str_len(buffer);
+		buffer[b_idx] = *(env[key_idx] + env_value);
+		b_idx++;
+		env_value++;
+	}
+	if (*(env[key_idx] + (env_value - 1)) == ':')
+	{
+		b_idx = b_idx - 2;
+		for (cwd_idx = 0; curr[cwd_idx] != '\0'; cwd_idx++)
+		{
+			buffer[b_idx] = curr[cwd_idx];
+			b_idx++;
+		}
+	}
+	free(curr);
+	return (buffer); }
---
+++ core_functionality/read_command.c
@@ -0,0 +1,29 @@
+#include "sash.h"
+
+/**
+ * read_line - Reads a line given to the standard input & stores it in a buffer
+ *
+ * Return: A pointer to the stored string
+ */
+char *read_line()
+{
+	char *str = NULL;
+	size_t size = 0;
+	ssize_t length = 0;
+
+	while ((length = getline(&str, &size, stdin)) != EOF)
+	{
+		if ((str == NULL) || (length < 2))
+		{
+			free(str);
+			return (NULL);
+		}
+
+		str[length - 1] = '\0';
+		return (str);
+	}
+	free(str);
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	_exit(0);
+}
